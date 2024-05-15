517d13517ed3515aecf5c7af753a9e029a1b28ac
Alexander Polakov <alex@plhk.dev>
2024-05-15 16:19:19 +0400

    feat(indexer): start from last transaction in db

--- db/src/lib.rs
+++ db/src/lib.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+
+use common::solana_sdk::signature::Signature;
 use common::types::{NeonTxInfo, SolanaBlock};
 use sqlx::PgPool;
 
@@ -164,6 +167,56 @@ impl TransactionRepo {
         .await?;
         txn.commit().await?;
         Ok(())
+    }
+}
+
+pub struct SolanaSignaturesRepo {
+    pool: sqlx::PgPool,
+}
+
+impl SolanaSignaturesRepo {
+    pub fn new(pool: sqlx::PgPool) -> Self {
+        Self { pool }
+    }
+
+    pub async fn insert(
+        &self,
+        slot: u64,
+        tx_idx: u64,
+        signature: Signature,
+    ) -> Result<(), sqlx::Error> {
+        sqlx::query!(
+            r#"
+            INSERT INTO solana_transaction_signatures
+            (
+                block_slot,
+                tx_idx,
+                signature
+            ) VALUES($1, $2, $3)
+            "#,
+            slot as i64,
+            tx_idx as i32,
+            signature.to_string()
+        )
+        .execute(&self.pool)
+        .await?;
+        Ok(())
+    }
+
+    pub async fn get_latest(&self) -> Result<Option<Signature>, sqlx::Error> {
+        let row = sqlx::query!(
+            r#"
+            SELECT signature as "signature!" FROM solana_transaction_signatures
+            ORDER BY block_slot DESC, tx_idx DESC
+            LIMIT 1
+            "#
+        )
+        .fetch_optional(&self.pool)
+        .await?;
+        match row {
+            Some(row) => Ok(Some(Signature::from_str(&row.signature).unwrap())),
+            None => Ok(None),
+        }
     }
 }
 
