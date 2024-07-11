bb7ae7847ff66012d73599c37e5f5f6b8356398d
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2024-07-11 16:58:17 +0300

    fix(proxy): make pyth symbology optional

--- proxy/src/mempool/gas_prices.rs
+++ proxy/src/mempool/gas_prices.rs
@@ -33,6 +33,7 @@ pub struct GasPricesConfig {
 pub struct GasPrices {
     prices: Arc<DashMap<Pubkey, Price>>,
     price_calculator: GasPriceCalculator,
+    const_gas_price: Option<u128>,
     base_token_pkey: Pubkey,
     default_token_pkey: Pubkey,
 }
@@ -52,66 +53,88 @@ impl GasPrices {
         let prices_thread = Arc::clone(&prices);
         let ws_url_thread = config.ws_url;
 
-        let base_token_pkey = *symbology
-            .get(&config.base_token)
-            .ok_or(MempoolError::BaseTokenNotFound(config.base_token))?;
-        let default_token_pkey = *symbology
-            .get(&config.default_token)
-            .ok_or(MempoolError::DefaultTokenNotFound(config.default_token))?;
+        let (base_token_pkey, default_token_pkey) = if calculator_config.const_gas_price.is_some() {
+            info!("using const_gas_price");
+            (
+                symbology
+                    .get(&config.base_token)
+                    .copied()
+                    .unwrap_or_default(),
+                symbology
+                    .get(&config.default_token)
+                    .copied()
+                    .unwrap_or_default(),
+            )
+        } else {
+            (
+                *symbology
+                    .get(&config.base_token)
+                    .ok_or(MempoolError::BaseTokenNotFound(config.base_token))?,
+                *symbology
+                    .get(&config.default_token)
+                    .ok_or(MempoolError::DefaultTokenNotFound(config.default_token))?,
+            )
+        };
 
-        tokio::spawn(async move {
-            let mut collector =
-                PythPricesCollector::try_new(&ws_url_thread, Arc::downgrade(&prices_thread))
-                    .await
-                    .expect("failed to create PythPricesCollector");
+        println!("symbology: {:?}", symbology);
+        if !symbology.is_empty() {
+            tokio::spawn(async move {
+                let mut collector =
+                    PythPricesCollector::try_new(&ws_url_thread, Arc::downgrade(&prices_thread))
+                        .await
+                        .expect("failed to create PythPricesCollector");
 
-            let refresh_rate = Duration::from_secs(EVM_CONFIG_REFRESH_RATE_SEC);
-            let mut interval = tokio::time::interval(refresh_rate);
-            let mut evm_tokens = HashSet::new();
-            loop {
-                let evm_config = match neon_api.get_config().await {
-                    Ok(config) => config,
-                    Err(err) => {
-                        error!(?err, "failed to get EVM config");
-                        interval.tick().await;
-                        continue;
-                    }
-                };
-
-                let new_tokens = evm_config
-                    .chains
-                    .iter()
-                    .map(|chain| chain.name.to_uppercase())
-                    .collect::<HashSet<_>>();
-                for token in new_tokens.difference(&evm_tokens) {
-                    if let Some(token_pkey) = symbology.get(token) {
-                        info!(?token, "subscribing to price");
-                        if let Err(err) = collector.subscribe(*token_pkey).await {
-                            error!(?err, "failed to subscribe to price");
+                let refresh_rate = Duration::from_secs(EVM_CONFIG_REFRESH_RATE_SEC);
+                let mut interval = tokio::time::interval(refresh_rate);
+                let mut evm_tokens = HashSet::new();
+                loop {
+                    let evm_config = match neon_api.get_config().await {
+                        Ok(config) => config,
+                        Err(err) => {
+                            error!(?err, "failed to get EVM config");
+                            interval.tick().await;
+                            continue;
                         }
-                    } else {
-                        warn!(?token, "subscribing to price: not found in symbology");
+                    };
+
+                    let new_tokens = evm_config
+                        .chains
+                        .iter()
+                        .map(|chain| chain.name.to_uppercase())
+                        .collect::<HashSet<_>>();
+                    for token in new_tokens.difference(&evm_tokens) {
+                        if let Some(token_pkey) = symbology.get(token) {
+                            info!(?token, "subscribing to price");
+                            if let Err(err) = collector.subscribe(*token_pkey).await {
+                                error!(?err, "failed to subscribe to price");
+                            }
+                        } else {
+                            warn!(?token, "subscribing to price: not found in symbology");
+                        }
                     }
-                }
-                for token in evm_tokens.difference(&new_tokens) {
-                    if let Some(token_pkey) = symbology.get(token) {
-                        info!(?token, "unsubscribing from price");
-                        collector.unsubscribe(*token_pkey).await;
-                    } else {
-                        warn!(
-                            ?token,
-                            "unsubscribing from to price: not found in symbology"
-                        );
+                    for token in evm_tokens.difference(&new_tokens) {
+                        if let Some(token_pkey) = symbology.get(token) {
+                            info!(?token, "unsubscribing from price");
+                            collector.unsubscribe(*token_pkey).await;
+                        } else {
+                            warn!(
+                                ?token,
+                                "unsubscribing from to price: not found in symbology"
+                            );
+                        }
                     }
+                    evm_tokens = new_tokens;
+                    interval.tick().await;
                 }
-                evm_tokens = new_tokens;
-                interval.tick().await;
-            }
-        });
+            });
+        }
+
+        let const_gas_price = calculator_config.const_gas_price();
 
         Ok(Self {
             prices,
             price_calculator: GasPriceCalculator::new(calculator_config),
+            const_gas_price,
             base_token_pkey,
             default_token_pkey,
         })
@@ -120,6 +143,10 @@ impl GasPrices {
     /// Get the gas price for the default token, or 0 if the price is not available.
     /// Precision is 18 decimal places.
     pub fn get_gas_price(&self) -> u128 {
+        if let Some(const_gas_price) = self.const_gas_price {
+            return const_gas_price;
+        }
+
         self.get_gas_for_token_pkey(&self.default_token_pkey)
             .unwrap_or(0)
     }
