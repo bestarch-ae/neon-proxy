c52a1c2b56bffab2e1ce54dc4981ed01fd9c864c
Alexander Polakov <alex@plhk.dev>
2024-08-15 12:39:40 +0400

    try finalize get balance

--- proxy/src/rpc.rs
+++ proxy/src/rpc.rs
@@ -387,7 +387,7 @@ impl EthApiServer for EthApiImpl {
         let tag = if let Some(block_number) = block_number {
             Some(self.get_tag_by_block_id(block_number).await?)
         } else {
-            None
+            BlockNumberOrTag::Finalized
         };
 
         let balance_address = BalanceAddress {
