107e1b45d9d0c3928ad99693f93404c47fd7a00b
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2024-09-29 22:02:08 +0100

    feat(mempool): add gas price model

--- mempool/src/gas_price_calculator.rs
+++ mempool/src/gas_price_calculator.rs
@@ -1,8 +1,34 @@
-#[derive(Debug, Clone)]
+use std::collections::VecDeque;
+
+use clap::Args;
+use reth_primitives::{U128, U64};
+
+use crate::gas_prices::{GasPriceModel, PRICES_REFRESH_RATE_MS};
+
+#[derive(Args, Debug, Clone)]
 pub struct GasPriceCalculatorConfig {
+    #[arg(long, default_value = "10000")]
     pub operator_fee: u128,
+    #[arg(long, default_value = "1")]
     pub min_gas_price: u128,
+    #[arg(long)]
     pub const_gas_price: Option<u128>,
+    #[arg(long, default_value = "10")]
+    pub gas_price_min_window: u64,
+    #[arg(long, default_value = "1000000000")]
+    pub min_acceptable_gas_price: u128,
+    #[arg(long, default_value = "15000")]
+    pub solana_cu_priority_fee: u128,
+    #[arg(long, default_value = "10000")]
+    pub solana_simple_cu_priority_fee: u128,
+    #[arg(long, default_value = "100000000000")]
+    pub min_wo_chain_id_acceptable_gas_price: u128,
+}
+
+impl GasPriceCalculatorConfig {
+    pub fn min_exec_gas_price_cnt(&self) -> usize {
+        (60000 / PRICES_REFRESH_RATE_MS * self.gas_price_min_window * 60000) as usize
+    }
 }
 
 impl GasPriceCalculatorConfig {
@@ -17,38 +43,89 @@ impl GasPriceCalculatorConfig {
 /// Calculates the gas price based on the base token price and the token price.
 #[derive(Debug, Clone)]
 pub struct GasPriceCalculator {
-    config: GasPriceCalculatorConfig,
+    pub config: GasPriceCalculatorConfig,
+    net_prices: VecDeque<u128>,
 }
 
 impl GasPriceCalculator {
     pub fn new(config: GasPriceCalculatorConfig) -> Self {
-        Self { config }
+        Self {
+            config,
+            net_prices: VecDeque::new(),
+        }
     }
 
-    /// Calculates the gas price based on the base token price and the token price.
-    /// Returns `None` if the token price is zero.
-    /// The formula is `net_price = base_price_usd / token_price_usd`.
-    /// The suggested price is `suggested_price = net_price * (1 + operator_fee)`.
-    /// If `const_gas_price` is set, it will be returned.
-    #[inline]
-    pub fn calculate_gas_price(&self, base_price_usd: u128, token_price_usd: u128) -> Option<u128> {
-        tracing::info!(base_price_usd, token_price_usd, "calculate_gas_price");
+    pub fn build_gas_price_model(
+        &mut self,
+        chain_id: u64,
+        token_name: String,
+        base_price_usd: u128,
+        token_price_usd: u128,
+    ) -> Option<GasPriceModel> {
         if let Some(const_gas_price) = self.config.const_gas_price() {
-            return Some(const_gas_price);
+            return Some(build_gas_price_model(
+                chain_id,
+                token_name,
+                true,
+                const_gas_price,
+                self.config.min_acceptable_gas_price,
+                base_price_usd,
+                token_price_usd,
+                &self.config,
+            ));
         }
 
         if token_price_usd == 0 {
             return None;
         }
-
         let net_price = base_price_usd * 1_000_000_000 / token_price_usd;
-        let suggested_price = net_price * (100_000 + self.config.operator_fee) / 100_000;
-        tracing::info!(
-            net_price,
-            suggested_price,
-            "suggested_price = ({base_price_usd} * 10^9 / {token_price_usd}) * (10^5 + {}) / 10^5",
-            self.config.operator_fee
-        );
-        Some(suggested_price)
+        self.net_prices.push_back(net_price);
+        if self.net_prices.len() > self.config.min_exec_gas_price_cnt() {
+            self.net_prices.pop_front();
+        }
+        let suggested_gas_price = net_price * (100_000 + self.config.operator_fee) / 100_000;
+        let min_executable_gas_price = self.net_prices.iter().min().copied().unwrap_or(0);
+
+        Some(build_gas_price_model(
+            chain_id,
+            token_name,
+            false,
+            suggested_gas_price,
+            min_executable_gas_price,
+            base_price_usd,
+            token_price_usd,
+            &self.config,
+        ))
+    }
+}
+
+#[allow(clippy::too_many_arguments)]
+#[inline]
+fn build_gas_price_model(
+    chain_id: u64,
+    token_name: String,
+    is_const_gas_price: bool,
+    suggested_gas_price: u128,
+    min_executable_gas_price: u128,
+    base_price_usd: u128,
+    token_price_usd: u128,
+    config: &GasPriceCalculatorConfig,
+) -> GasPriceModel {
+    GasPriceModel {
+        token_name,
+        chain_id: U64::from(chain_id),
+        gas_price: U128::from(suggested_gas_price),
+        suggested_gas_price: U128::from(suggested_gas_price),
+        is_const_gas_price,
+        min_acceptable_gas_price: U128::from(config.min_acceptable_gas_price),
+        min_executable_gas_price: U128::from(min_executable_gas_price),
+        chain_token_price_usd: U128::from(base_price_usd),
+        token_price_usd: U128::from(token_price_usd),
+        operator_fee: U128::from(config.operator_fee),
+        solana_cu_priority_fee: U128::from(config.solana_cu_priority_fee),
+        solana_simple_cu_priority_fee: U128::from(config.solana_simple_cu_priority_fee),
+        min_wo_chain_id_acceptable_gas_price: U128::from(
+            config.min_wo_chain_id_acceptable_gas_price,
+        ),
     }
 }
