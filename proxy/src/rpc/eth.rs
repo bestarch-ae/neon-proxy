1685706761e502235f7d494b2ea9eaa2470f7472
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2024-10-11 16:51:19 +0100

    debug(proxy)

--- proxy/src/rpc/eth.rs
+++ proxy/src/rpc/eth.rs
@@ -462,6 +462,7 @@ impl EthApiServer for EthApiImpl {
 
         let tag = if let Some(block_number) = block_number {
             Some(self.get_tag_by_block_id(block_number).await?)
+        } else {
             None
         };
 
