1b1094a0f6a9502626a90ae8cd16d664c163e910
Nikita Podoliako <00nktk@hey.com>
2024-10-17 13:51:17 +0300

    feat(operator): multi-operator execution
    refactor(operator): Operators -> OperatorPool
    
    refactor: ExecutorTrait -> Execute
    
    refactor(operator): move OperatorPool to a separate crate

--- operator/src/error.rs
+++ operator/src/error.rs
@@ -1,10 +1,7 @@
-use reth_primitives::Address;
 use thiserror::Error;
 
 #[derive(Debug, Error)]
 pub enum Error {
-    #[error("unknown operator")]
-    UnknownOperator(Address),
     #[error("signature error: {0}")]
     Signature(#[from] alloy_signer::Error),
     #[error("load error: {0}")]
