c01110ffcd24dec29d9e950629bdb3d3f33be048
Nikita Podoliako <00nktk@hey.com>
2024-10-25 15:33:42 +0300

    refactor(executor): move error handling implementation to preflight_error module

--- executor/src/transactions/preflight_error.rs
+++ executor/src/transactions/preflight_error.rs
@@ -64,3 +64,143 @@ fn extract_logs(err: &ClientErrorKind) -> &'_ [String] {
     }
 }
 
+/// ## Preflight error handling implementations
+impl TransactionBuilder {
+    /// Retry exceeded CU Meter error
+    pub(super) fn handle_cu_meter(&self, tx: OngoingTransaction) -> Result<OngoingTransaction> {
+        let tx_hash = tx.eth_tx().map(|tx| tx.tx_hash()).copied();
+        if tx.heap_frame().map_or(false, |n| n == MAX_HEAP_SIZE)
+            && tx.cu_limit().map_or(false, |n| n == MAX_COMPUTE_UNITS)
+        {
+            bail!("CU Meter exceeded for transaction with maximum budget")
+        }
+
+        let mut tx = tx;
+        if tx.heap_frame().map_or(true, |n| n != MAX_HEAP_SIZE) {
+            tracing::warn!(?tx_hash, "retry with max heap");
+            tx = tx
+                .with_heap_frame(MAX_HEAP_SIZE)
+                .context("cannot set heap frame")?;
+        }
+
+        if tx.cu_limit().map_or(true, |n| n != MAX_COMPUTE_UNITS) {
+            tracing::warn!(?tx_hash, "retry with max CU");
+            tx = tx
+                .with_cu_limit(MAX_HEAP_SIZE)
+                .context("cannot set compute units")?;
+        }
+        Ok(tx)
+    }
+
+    /// Retry exceeded Transaction size error
+    pub(super) async fn handle_tx_size(
+        &self,
+        tx: OngoingTransaction,
+    ) -> Result<OngoingTransaction> {
+        let tx_hash = tx.eth_tx().map(|tx| tx.tx_hash()).copied();
+        match tx.disassemble() {
+            TxStage::Final {
+                tx_data: Some(tx_data),
+                holder: Some(holder),
+            }
+            | TxStage::DataExecution {
+                tx_data,
+                holder,
+                alt: None,
+                ..
+            }
+            | TxStage::IterativeExecution {
+                tx_data,
+                holder,
+                alt: None,
+                ..
+            } => {
+                tracing::warn!(?tx_hash, "tx retry with ALT");
+                self.start_from_alt(tx_data, Some(holder)).await
+            }
+            TxStage::DataExecution {
+                tx_data,
+                alt: Some(_),
+                ..
+            }
+            | TxStage::IterativeExecution {
+                tx_data,
+                alt: Some(_),
+                ..
+            }
+            | TxStage::Final {
+                tx_data: Some(tx_data),
+                holder: None,
+            } => {
+                tracing::warn!(?tx_hash, "Data tx retry from Holder");
+                self.start_holder_execution(tx_data.envelope).await
+            }
+            stage @ TxStage::HolderFill { .. }
+            | stage @ TxStage::AltFill { .. }
+            | stage @ TxStage::Final { tx_data: None, .. }
+            | stage @ TxStage::RecoveredHolder { .. }
+            | stage @ TxStage::Cancel { .. } => bail!("cannot shorten tx size: {stage:?}"),
+        }
+    }
+
+    /// Retry ALT program error
+    pub(super) async fn handle_alt(&self, tx: OngoingTransaction) -> Result<OngoingTransaction> {
+        match tx.disassemble() {
+            TxStage::AltFill {
+                info,
+                tx_data,
+                holder,
+            } => {
+                let (info, ix) = self.alt_mgr.recreate_alt(info).await?;
+                Ok(TxStage::alt_fill(info, tx_data, holder).ongoing(&[ix], &self.pubkey()))
+            }
+            stage => unreachable!("only alt stages can fail with AltFail: {stage:?}"),
+        }
+    }
+
+    /// Retry External call fail error
+    pub(super) async fn handle_ext_call(
+        &self,
+        tx: OngoingTransaction,
+    ) -> Result<OngoingTransaction> {
+        let tx_hash = tx.eth_tx().map(|tx| tx.tx_hash()).copied();
+
+        let alt = tx.alt().cloned();
+        match tx.disassemble() {
+            TxStage::DataExecution { tx_data, .. }
+            | TxStage::Final {
+                tx_data: Some(tx_data),
+                holder: None,
+            } => {
+                // This will probably result in iterative fallback with cancel
+                tracing::warn!(?tx_hash, "Data tx retry from Holder");
+                // TODO: Maybe fallback to data iterative?
+                //     : But we need to init holder anyway
+                self.start_holder_execution(tx_data.envelope).await
+            }
+            TxStage::Final {
+                tx_data: Some(tx_data),
+                holder: Some(holder),
+            } => {
+                tracing::warn!(?tx_hash, "Fallback to iterative");
+                self.step(None, tx_data, holder, false, alt.clone())
+                    .await
+                    .transpose()
+                    .expect("always some for first iter")
+            }
+            TxStage::IterativeExecution {
+                tx_data, holder, ..
+            } => {
+                tracing::warn!(?tx_hash, "Cancelling transaction");
+                self.cancel(tx_data, holder, alt)
+            }
+            stage @ TxStage::HolderFill { .. }
+            | stage @ TxStage::AltFill { .. }
+            | stage @ TxStage::Final { .. }
+            | stage @ TxStage::RecoveredHolder { .. }
+            | stage @ TxStage::Cancel { .. } => {
+                bail!("{stage:?} cannot fallback to iterative")
+            }
+        }
+    }
+}
