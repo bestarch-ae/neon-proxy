bede9b4410889b9b6d1e55cb86aeec71f29d6aff
Timothy N. Tsvetkov <timothy.tsvetkov@gmail.com>
2024-11-04 19:44:42 +0000

    oops (squash)

--- schemas/migrations/04_fix_table_types_and_constraints.sql
+++ schemas/migrations/04_fix_table_types_and_constraints.sql
@@ -4,7 +4,7 @@ ADD CONSTRAINT sol_sig_length_check CHECK (octet_length(sol_sig) = 64);
 
 ALTER TABLE solana_transaction_costs
 ALTER COLUMN sol_sig TYPE BYTEA USING sol_sig::BYTEA,
-ALTER COLUMN operator TYPE BYTEA USING operator::BYTEA;
+ALTER COLUMN operator TYPE BYTEA USING operator::BYTEA,
 ADD CONSTRAINT sol_sig_length_check CHECK (octet_length(sol_sig) = 64),
 ADD CONSTRAINT unique_sol_sig UNIQUE (sol_sig);
 
