29d381e222dd11cbb5e756463ea119e10fd7f638
Nikita Podoliako <00nktk@hey.com>
2024-11-19 23:24:02 +0300

    tests(mempool): add proptests

--- mempool/src/lib.rs
+++ mempool/src/lib.rs
@@ -10,5 +10,6 @@ pub use error::MempoolError;
 pub use gas_price_calculator::GasPriceCalculatorConfig;
 pub use gas_prices::{GasPriceModel, GasPrices, GasPricesConfig, GasPricesTrait};
 pub use mempool::{Config as MempoolConfig, Mempool};
+pub use pools::EXEC_INTERVAL_MS;
 pub use pyth_price_collector::pyth_collect_symbology;
 pub use validator::{PreFlightError, PreFlightValidator};
